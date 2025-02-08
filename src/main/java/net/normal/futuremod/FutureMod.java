72303889a6f641daeb753cc17dc8ecb1e5a4cc5c
Vinicius de Oliveira Silva <vinyoliveira2005@gmail.com>
2025-02-08 18:50:24 -0300

    first commit

---
+++ src/main/java/net/normal/futuremod/FutureMod.java
@@ -0,0 +1,16 @@
+package net.normal.futuremod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FutureMod implements ModInitializer {
+	public static final String MOD_ID = "futuremod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/normal/futuremod/FutureModClient.java
@@ -0,0 +1,10 @@
+package net.normal.futuremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FutureModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
