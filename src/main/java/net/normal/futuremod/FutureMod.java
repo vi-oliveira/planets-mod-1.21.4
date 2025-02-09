1c86a62945a866ee0d4c99248566bec95e7d49d9
Vinicius de Oliveira Silva <vinyoliveira2005@gmail.com>
2025-02-08 23:04:07 -0300

    adding the first item, the happy potato

--- src/main/java/net/normal/futuremod/FutureMod.java
+++ src/main/java/net/normal/futuremod/FutureMod.java
@@ -2,6 +2,7 @@ package net.normal.futuremod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.normal.futuremod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class FutureMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/normal/futuremod/item/ModItems.java
+++ src/main/java/net/normal/futuremod/item/ModItems.java
@@ -1,5 +1,29 @@
 package net.normal.futuremod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.normal.futuremod.FutureMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
 
+    public static final Item HAPPY_POTATO = registerItem("happy_potato", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FutureMod.MOD_ID,"happy_potato")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(FutureMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        FutureMod.LOGGER.info("Registering Mod Items for " + FutureMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+           entries.add(HAPPY_POTATO);
+        });
+    }
 }
